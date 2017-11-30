98047b74c44cc851d6dd512f8a1d35b94f5af1ce
marshalpaterson <marshal.paterson@tquilaanz.com>
2017-11-30 14:09:58 +1100

    Fixed stylesheet issue

--- tq-anz-property-services/tq-anz-building-height/index.js
+++ tq-anz-property-services/tq-anz-building-height/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, styles } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import FiestaPicker from '../../tq-anz-picker';
 
 class TqanzPropertyServicesBuildingHeight extends Component {
