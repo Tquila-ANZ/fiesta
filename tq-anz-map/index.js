7c5318d8bf265e552b9c42e036f427311e2e9c58
marshalpaterson <marshal.paterson@gmail.com>
2017-12-15 14:30:52 +1100

    Added map

--- tq-anz-map/index.js
+++ tq-anz-map/index.js
@@ -1,53 +1,127 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Dimensions,
-  ScrollView,
-} from 'react-native';
+// import React from 'react';
+// import {
+//   StyleSheet,
+//   Dimensions,
+//   ScrollView,
+// } from 'react-native';
 
-import MapView from 'react-native-maps';
+// import MapView from 'react-native-maps';
 
-const { width, height } = Dimensions.get('window');
+// const { width, height } = Dimensions.get('window');
 
+// const ASPECT_RATIO = width / height;
+// const LATITUDE = 37.78825;
+// const LONGITUDE = -122.4324;
+// const LATITUDE_DELTA = 0.0922;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+// const SAMPLE_REGION = {
+//   latitude: LATITUDE,
+//   longitude: LONGITUDE,
+//   latitudeDelta: LATITUDE_DELTA,
+//   longitudeDelta: LONGITUDE_DELTA,
+// };
+
+// class TqanzMap extends React.Component {
+//   render() {
+//     const maps = [];
+//     for (let i = 0; i < 10; i++) {
+//       maps.push(
+//         <MapView
+//           liteMode
+//           key={`map_${i}`}
+//           style={styles.map}
+//           initialRegion={SAMPLE_REGION}
+//         />
+//       );
+//     }
+//     return (
+//       <ScrollView style={StyleSheet.absoluteFillObject}>
+//         {maps}
+//       </ScrollView>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   map: {
+//     height: 200,
+//     marginVertical: 50,
+//   },
+// });
+
+// module.exports = TqanzMap;
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+let { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
+const LATITUDE = 0;
+const LONGITUDE = 0;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-const SAMPLE_REGION = {
-  latitude: LATITUDE,
-  longitude: LONGITUDE,
-  latitudeDelta: LATITUDE_DELTA,
-  longitudeDelta: LONGITUDE_DELTA,
-};
-
-class TqanzMap extends React.Component {
-  render() {
-    const maps = [];
-    for (let i = 0; i < 10; i++) {
-      maps.push(
-        <MapView
-          liteMode
-          key={`map_${i}`}
-          style={styles.map}
-          initialRegion={SAMPLE_REGION}
-        />
-      );
-    }
-    return (
-      <ScrollView style={StyleSheet.absoluteFillObject}>
-        {maps}
-      </ScrollView>
-    );
-  }
+export default class TqanzMap extends Component {
+	constructor() {
+		super();
+		this.state = {
+			region: {
+				latitude: LATITUDE,
+				longitude: LONGITUDE,
+				latitudeDelta: LATITUDE_DELTA,
+				longitudeDelta: LONGITUDE_DELTA
+			}
+		};
+	}
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				this.setState({
+					region: {
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude,
+						latitudeDelta: LATITUDE_DELTA,
+						longitudeDelta: LONGITUDE_DELTA
+					}
+				});
+			},
+			error => console.log(error.message),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+		);
+		this.watchID = navigator.geolocation.watchPosition(position => {
+			this.setState({
+				region: {
+					latitude: position.coords.latitude,
+					longitude: position.coords.longitude,
+					latitudeDelta: LATITUDE_DELTA,
+					longitudeDelta: LONGITUDE_DELTA
+				}
+			});
+		});
+	}
+	componentWillUnmount() {
+		navigator.geolocation.clearWatch(this.watchID);
+	}
+	render() {
+		return (
+			<MapView
+				provider={PROVIDER_GOOGLE}
+				style={styles.container}
+				showsUserLocation={true}
+				region={this.state.region}
+				onRegionChange={region => this.setState({ region })}
+				onRegionChangeComplete={region => this.setState({ region })}
+			>
+				<MapView.Marker coordinate={this.state.region} />
+			</MapView>
+		);
+	}
 }
-
 const styles = StyleSheet.create({
-  map: {
-    height: 200,
-    marginVertical: 50,
-  },
+	container: {
+		height: '100%',
+		width: '100%'
+	}
 });
-
-module.exports = TqanzMap;
+// AppRegistry.registerComponent('MapExample', () => MapExample);
