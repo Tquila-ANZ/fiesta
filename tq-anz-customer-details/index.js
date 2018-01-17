db0dfb24d7d0c88e58f9bf874a949760db63ec0d
marshalpaterson <marshal.paterson@gmail.com>
2018-01-17 11:58:20 +1100

    Added some form

--- tq-anz-customer-details/index.js
+++ tq-anz-customer-details/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import {Hr} from 'react-native-hr';
 import styles from './styles';
 
 class TqanzCustomerDetails extends Component {
@@ -10,16 +11,22 @@ class TqanzCustomerDetails extends Component {
 	render() {
 		return (
 			<View style={styles.inputsContainer}>
-				<View style={{ height: 8 }}><Text>First name</Text></View>
-				<View style={{ height: 8 }}><Text>{this.props.data["FirstName"]}</Text></View>
-				<View style={{ height: 8 }}><Text>Surname</Text></View>
-				<View style={{ height: 8 }}><Text>{this.props.data["LastName"]}</Text></View>
-				<View style={{ height: 8 }}><Text>Gender</Text></View>
-				<View style={{ height: 8 }}><Text>{this.props.data["Gender__c"]}</Text></View>
-				<View style={{ height: 8 }}><Text>Date of Birth</Text></View>
-				<View style={{ height: 8 }}><Text>{this.props.data["Date_of_Birth__c"]}</Text></View>
-				<View style={{ height: 8 }}><Text>mobile</Text></View>
-				<View style={{ height: 8 }}><Text>{this.props.data["MobilePhone"]}</Text></View>
+			<View style={{flexDirection: 'row', flex: 1}}>
+				<View><Text>First name</Text></View>
+				<View><Text>{this.props.data["FirstName"]}</Text></View>
+				<View><Text>Surname</Text></View>
+				<View><Text>{this.props.data["LastName"]}</Text></View>
+				</View>
+				<Hr />
+				<View style={{flexDirection: 'row', flex: 1}}>
+				<View><Text>Gender</Text></View>
+				<View><Text>{this.props.data["Gender__c"]}</Text></View>
+				<View><Text>Date of Birth</Text></View>
+				<View><Text>{this.props.data["Date_of_Birth__c"]}</Text></View>
+				</View>
+				<Hr />
+				<View><Text>mobile</Text></View>
+				<View><Text>{this.props.data["MobilePhone"]}</Text></View>
 			</View>
 		);
 	}
