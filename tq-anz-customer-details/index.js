95d6cc463a3bc7efd72d99d55a0c174a36dbb8a9
marshalpaterson <marshal.paterson@gmail.com>
2018-01-17 11:27:42 +1100

    Adding props to details

--- tq-anz-customer-details/index.js
+++ tq-anz-customer-details/index.js
@@ -5,24 +5,21 @@ import styles from './styles';
 class TqanzCustomerDetails extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {data: this.props.data};
+		this.state = {data: this.props.data, height: this.props.height, titleColor: this.props.titleColor, valueColor: this.props.valueColor};
 	}
 	render() {
-		// Based off a object only
-		const dataLoopObj = (data, keyName, uiName = null) => {
-			return (
-				<View>
-					<Text style={{ fontSize: 22 }}>
-						{uiName ? uiName : keyName}:  {data[keyName]}
-					</Text>
-				</View>
-			);
-		};
-
 		return (
 			<View style={styles.inputsContainer}>
-				{dataLoopObj(this.props.data, this.props.keyName, this.props.uiName)}
-				<View style={{ height: 8 }} />
+				<View style={{ height: 8 }}><Text>First name</Text></View>
+				<View style={{ height: 8 }}><Text>{this.props.data["FirstName"]}</Text></View>
+				<View style={{ height: 8 }}><Text>Surname</Text></View>
+				<View style={{ height: 8 }}><Text>{this.props.data["LastName"]}</Text></View>
+				<View style={{ height: 8 }}><Text>Gender</Text></View>
+				<View style={{ height: 8 }}><Text>{this.props.data["Gender__c"]}</Text></View>
+				<View style={{ height: 8 }}><Text>Date of Birth</Text></View>
+				<View style={{ height: 8 }}><Text>{this.props.data["Date_of_Birth__c"]}</Text></View>
+				<View style={{ height: 8 }}><Text>mobile</Text></View>
+				<View style={{ height: 8 }}><Text>{this.props.data["MobilePhone"]}</Text></View>
 			</View>
 		);
 	}
