46165658c9735956e768671171c7d9b6b6b17faa
marshalpaterson <marshal.paterson@tquilaanz.com>
2018-02-09 14:21:42 +1100

    Just added the ability to use for SF store

---
+++ tq-anz-offline/index.js
@@ -0,0 +1,38 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { smartsync } from 'react-native-force';
+export default class TqanzOffline {
+    constructor() {
+    }
+    syncDown(callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            smartsync.syncDown(callback);
+            return [];
+        });
+    }
+    reSync(callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            smartsync.reSync(callback);
+            return [];
+        });
+    }
+    syncUp(callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            smartsync.syncUp(callback);
+            return [];
+        });
+    }
+    syncData(callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            smartsync.syncData(callback);
+            return [];
+        });
+    }
+}
+//# sourceMappingURL=index.js.map
