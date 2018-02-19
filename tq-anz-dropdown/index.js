46ac9fb8130e549529dc65a6e6323a128af298df
marshalpaterson <marshal.paterson@tquilaanz.com>
2018-02-19 13:36:22 +1100

    Added drop down

--- tq-anz-dropdown/index.js
+++ tq-anz-dropdown/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { View } from 'react-native';
 import DropDown from 'react-native-dropdown';
 const { Select, Option, updatePosition } = DropDown;
-export default class TqanzDropDown extends Component {
+export default class TqanzDropDown extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -13,18 +13,12 @@ export default class TqanzDropDown extends Component {
     componentDidMount() {
         updatePosition(this.refs['SELECT1']);
     }
-    _getOptionList() {
-        return this.refs['OPTIONLIST'];
-    }
-    _usa(state) {
-        this.setState(Object.assign({}, this.state, { usa: state }));
-    }
-    _canada(province) {
+    onChangeHandler(province) {
         this.setState(Object.assign({}, this.state, { canada: province }));
     }
     render() {
         return (React.createElement(View, { style: { flex: 1, justifyContent: 'center', alignItems: 'center' } },
-            React.createElement(Select, { width: 250, ref: "SELECT1", optionListRef: this._getOptionList.bind(this), defaultValue: "Select a Province in Canada ...", onSelect: this._canada.bind(this) },
+            React.createElement(Select, { width: 250, ref: "SELECT1", defaultValue: "Select ...", onSelect: this.onChangeHandler.bind(this) },
                 React.createElement(Option, null, "Alberta"),
                 React.createElement(Option, null, "British Columbia"),
                 React.createElement(Option, null, "Manitoba"),
--- tq-anz-radio-button/index.js
+++ tq-anz-radio-button/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import * as React from "react";
 import { View, Text } from "react-native";
 import { RadioGroup, RadioButton } from 'react-native-flexi-radio-button';
 class RadioGroupItem {
@@ -7,7 +7,7 @@ class RadioGroupItem {
         this.text = text;
     }
 }
-class TqanzRadioButton extends Component {
+class TqanzRadioButton extends React.Component {
     constructor(props) {
         super(props);
         this.renderGroup = (a) => {
