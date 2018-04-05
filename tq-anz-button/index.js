e575a6a03d6f9dfc0e46401a281a37b55a05d6c6
Shailendra <shailendrapal100@gmail.com>
2018-04-05 13:32:24 +1000

    Add icon to fiesta button

--- tq-anz-button/index.js
+++ tq-anz-button/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
+import FontAwesome, { Icons } from "react-native-fontawesome";
 
 class TqanzButton extends PureComponent {
   render() {
@@ -8,14 +9,18 @@ class TqanzButton extends PureComponent {
       ...this.props.styles
     };
 
-    let props = { ...this.props };
-    // No need for styles in the props anymore
-    delete props["styles"];
+    const { title = "Button", onPress = () => {}, icon = null } = this.props;
 
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} {...props}>
-          <Text style={styles.text}>{props.name}</Text>
+        <TouchableOpacity
+          onPress={onPress}
+          style={[styles.container, styles.button]}
+        >
+          {icon ? (
+            <FontAwesome style={styles.icon}>{Icons[icon]}</FontAwesome>
+          ) : null}
+          <Text style={styles.text}>{title}</Text>
         </TouchableOpacity>
       </View>
     );
@@ -25,6 +30,28 @@ class TqanzButton extends PureComponent {
 const defaultStyles = {
   container: {},
   button: {},
+  icon: {},
+  text: {}
+};
+
+const roundButtonWithIconOnTop = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    borderRadius: 100,
+    margin: 10,
+    borderWidth: 2,
+    borderColor: "blue",
+    height: 100,
+    width: 100
+  },
+  icon: {
+    fontSize: 24,
+    color: "blue"
+  },
   text: {}
 };
 
