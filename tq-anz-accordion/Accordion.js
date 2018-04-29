355f6204d298e370e1a5365cdd63e2ca333d36f8
shailjo <shailendra.pal@tquilaanz.com>
2018-04-29 13:41:54 +1000

    Remove redundant proptypes

--- tq-anz-accordion/Accordion.js
+++ tq-anz-accordion/Accordion.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from "react";
+import React, { Component } from "react";
 import { StyleSheet, View, TouchableHighlight } from "react-native";
 
 import Collapse from "./Collapse";
--- tq-anz-accordion/Collapse.js
+++ tq-anz-accordion/Collapse.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from "react";
+import React, { Component } from "react";
 import { StyleSheet, Animated, ScrollView } from "react-native";
 
 export default class Collapse extends Component {
