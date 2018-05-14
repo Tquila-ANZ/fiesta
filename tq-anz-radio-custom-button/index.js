9c733044a43a3a1c062d8fe937cb2633d76f006d
jordano <shailendrapal100@gmail.com>
2018-05-14 12:31:38 +1000

    Add custom radio button

---
+++ tq-anz-radio-custom-button/index.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import RadioGroup from "./radioGroup";
+import RadioButton from "./radioButton";
+
+class TqanzRadioCustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      radios: props.radios,
+      styles: {
+        ...defaultStyles,
+        ...props.styles
+      }
+    };
+  }
+
+  getIndex = (radios, selected) => {
+    if (!radios) {
+      return;
+    }
+    let index = undefined;
+
+    radios.map((radio, i) => {
+      if (radio.value === selected) {
+        index = i;
+      }
+    });
+
+    return index;
+  };
+
+  onSelect = (index, value) => {
+    // This is to provide the ability to deselect a radio button if it is tapped
+    // again
+    const { selectedIndex } = this.state;
+    if (index === selectedIndex) {
+      this.setState({ selectedIndex: null });
+      this.props.onSelect(index, null);
+    } else {
+      this.setState({ selectedIndex: index });
+      this.props.onSelect(index, value);
+    }
+  };
+
+  getTextColor = (index, selectedIndex) => {
+    if (index === selectedIndex) {
+      return { color: this.props.activeColor };
+    } else {
+      return {};
+    }
+  };
+
+  renderGroup = (a, selectedIndex) => {
+    if (a === undefined) a = new Array();
+
+    // FOR TESTING a.push(new RadioGroupItem(1, "Single")); a.push(new
+    // RadioGroupItem(1, "Double"));
+
+    return a.map((info, index) => (
+      <RadioButton
+        key={info.value}
+        value={info.value}
+        style={this.state.styles.radio_button}
+        activeColor={this.props.activeColor}
+        color={this.props.activeColor}
+      >
+        <Text
+          key={info.text}
+          style={[
+            this.state.styles.radio_text,
+            this.getTextColor(index, selectedIndex)
+          ]}
+        >
+          {info.text}
+        </Text>
+      </RadioButton>
+    ));
+  };
+
+  render() {
+    const styles = this.state.styles;
+    const index = this.getIndex(this.props.radios, this.props.selectedRadio);
+    const { inactiveColor = "black", renderHeader = () => {} } = this.props;
+    return (
+      <View>
+        <RadioGroup
+          onSelect={this.onSelect}
+          style={styles.radio_group}
+          renderHeader={renderHeader}
+          selectedIndex={index}
+          color={inactiveColor}
+        >
+          {this.renderGroup(this.props.radios, index)}
+        </RadioGroup>
+      </View>
+    );
+  }
+}
+
+const defaultStyles = {
+  radio_text: {},
+  radio_group: {},
+  radio_button: {}
+};
+
+export default TqanzRadioButton;
---
+++ tq-anz-radio-custom-button/radioButton.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
+
+export default class RadioButton extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ selectedIndex: nextProps.selectedIndex });
+  }
+
+  render() {
+    var { children, isSelected, renderHeader = () => {}, value } = this.props;
+
+    const styles = {
+      ...defaultStyles,
+      ...this.props.styles
+    };
+
+    return (
+      <View
+        style={{
+          opacity: this.props.disabled ? 0.4 : 1
+        }}
+      >
+        <TouchableWithoutFeedback
+          disabled={this.props.disabled}
+          onPress={() =>
+            this.context.onSelect(this.props.index, this.props.value)
+          }
+        >
+          <View
+            style={[
+              styles.container,
+              this.props.style,
+              this.props.isSelected
+                ? {
+                    backgroundColor: this.context.highlightColor
+                  }
+                : null
+            ]}
+          >
+            <View>{renderHeader(value, isSelected)}</View>
+            <View style={styles.item}>{children}</View>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+RadioButton.contextTypes = {
+  onSelect: PropTypes.func.isRequired,
+  size: PropTypes.number.isRequired,
+  thickness: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  highlightColor: PropTypes.string
+};
+
+let defaultStyles = {
+  container: {
+    flexGrow: 1,
+    flexDirection: "row",
+    padding: 10
+  },
+  radio: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  item: {
+    marginLeft: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  icon: {
+    fontSize: 14
+  }
+};
